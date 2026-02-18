9c1a1e4e9531c0d01724f7187b85bff0db56b108
ABISHEKADHITHI <nabishek21022004@gmail.com>
2026-02-18 16:30:23 +0530

    changes made in auth.controller

--- blogplatform/src/controllers/auth.controller.js
+++ blogplatform/src/controllers/auth.controller.js
@@ -16,11 +16,13 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
     const {email, password} = req.body
     const user = await User.findOne({ where : {email}})
-    if(!user) 
-        return res.status(400).json({ message: "User not found"})
+    
+    if(!user)  return res.status(400).json({ message: "User not found"})
+
     const valid = await bcrypt.compare(password, user.password)
-    if(!valid)
-        return res.status(400).json({ message: "Invalid password"})
+
+    if(!valid)  return res.status(400).json({ message: "Invalid password"})
+
     const token = jwt.sign(
     { id: user.id, role: user.role },
     process.env.JWT_SECRET
